9af7de9710b5e7c7312b2d7fe7649427449100d2
Artem <fedorzvyk@gmail.com>
2022-11-22 18:42:15 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,49 @@
+// let number = 5;
+
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// // leftBorderWidth = 10;
+
+// const obj = {
+//     a: 50rty
+// };
+
+// obj.a = 10;
+// console.log(obj);
+
+
+// var name = ' ivan';
+// console.log(name);
+
+// {
+    // var result = 53;
+// }
+// console.log(result);
+
+// alert('Hello');
+
+// const result = confirm('are you here');
+
+// const result = +prompt("18?", "18");
+// console.log(typeof (result));
+
+const answers = [];
+
+// answers[0] = prompt("Name", "");
+// answers[1] = prompt("surname", "");
+// answers[2] = prompt("year", "");
+
+// document.write(answers);
+
+console.log(typeof(null));
+
+let i = 10,
+    d = 10;
+
+console.log(i++);
+    console.log(d--);
+console.log(i);
+    console.log(d);
